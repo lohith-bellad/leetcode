65ab60de1f3ba20ca6038bd388af94c123c39215
lohith bellad <lohith.pre@gmail.com>
2025-03-13 11:21:50 -0700

    update

--- python/1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        output = ""
-        for c1, c2 in zip(word1, word2):
-            output += c1 + c2
-
-        output += word1[len(output)//2:] + word2[len(output)//2:]
-
-        return output
