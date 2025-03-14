474d8412d99530e1e1cc7cd46d04dc6b87b9d636
Lohith Bellad <lohith.pre@gmail.com>
2025-03-14 13:12:35 -0700

    Time: 11 ms (53.92%), Space: 35 MB (10.17%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hash_map = {}
+
+        for num in nums:
+            if num not in hash_map:
+                hash_map[num] = 1
+            else:
+                return True
+        
+        return False
