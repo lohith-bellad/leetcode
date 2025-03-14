a5ca3cc7df145b97deb69a3c4e7299010e6c6999
Lohith Bellad <lohith.pre@gmail.com>
2025-03-14 11:03:06 -0700

    Time: 2 ms (32.56%), Space: 17.9 MB (41.41%) - LeetHub

---
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.py
@@ -0,0 +1,22 @@
+class Solution:
+    @staticmethod
+    def signFunc(val):
+        if val < 0:
+            return -1
+        elif val > 0:
+            return 1
+        else:
+            return 0
+
+    def arraySign(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        product = 1
+
+        for num in nums:
+            product *= num
+        
+        return Solution.signFunc(product)
+        
