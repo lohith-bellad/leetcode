4f77ae4b993f625f7548c3176f21cea145bf9ca5
Lohith Bellad <lohith.pre@gmail.com>
2025-03-15 16:13:29 -0700

    Time: 15 ms (14.03%), Space: 18 MB (83.64%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        """
         temp = []
 
         for c in s:
@@ -8,15 +9,37 @@ class Solution:
                     temp.append(c.lower())
                 else:
                     temp.append(c)
-        
-        end = len(temp) - 1
+        """
+        end = len(s) - 1
         start = 0
 
+        temp = s
+        while start <= end and s[start].isalnum() == False:
+            start += 1
+        
+        while start <= end and s[end].isalnum() == False:
+            end -= 1
+
         while start <= end:
-            if temp[start] != temp[end]:
+            if temp[start].isalpha() and temp[end].isdigit():
+                return False
+
+            if temp[start].isdigit() and temp[end].isalpha():
+                return False
+
+            if temp[start].isdigit() and temp[start] != temp[end]:
+                return False
+            
+            if temp[start].isalpha() and temp[start].lower() != temp[end].lower():
                 return False
             
             start += 1
             end -= 1
-        
+
+            while start <= end and s[start].isalnum() == False:
+                start += 1
+
+            while start <= end and s[end].isalnum() == False:
+                end -= 1
+
         return True
