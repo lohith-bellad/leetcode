712323c01a45af46cfa4de5785298412cb752d94
Lohith Bellad <lohith.pre@gmail.com>
2025-03-14 19:51:00 -0700

    Time: 0 ms (100%), Space: 17.5 MB (99.91%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_hash = {}
+        col_hash = {}
+
+        for r in range(9):
+            row_hash.clear()
+            col_hash.clear()
+            for c in range(9):
+                if board[r][c] != ".":
+                    if board[r][c] not in row_hash:
+                        row_hash[board[r][c]] = 1
+                    else:
+                        return False
+                if board[c][r] != ".":
+                    if board[c][r] not in col_hash:
+                        col_hash[board[c][r]] = 1
+                    else:
+                        return False
+        
+        for outer_row in range(0, 9, 3):
+            for outer_col in range(0, 9, 3):
+                row_hash.clear()
+                for r in range(outer_row, outer_row + 3):
+                    for c in range(outer_col, outer_col + 3):
+                        if board[r][c] != ".":
+                            if board[r][c] not in row_hash:
+                                row_hash[board[r][c]] = 1
+                            else:
+                                return False
+
+        return True
