8d047d3190a1982d5ade762e73b732916c37f1e1
Lohith Bellad <lohith.pre@gmail.com>
2025-03-15 22:30:20 -0700

    Time: 3 ms (30.66%), Space: 18.2 MB (6.09%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,17 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        output = []
+
+        def build(open: int, close: int, path: str, output: []):
+            if open == 0 and close == 0:
+                output.append(path)
+
+            if open > 0:
+                build(open - 1, close, path + "(", output)
+            if close > open:
+                build(open, close - 1, path + ")", output)
+            return
+
+        build(n, n, "", output)
+        return output
+            
