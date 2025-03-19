b875c0263b7ef1c9a0b8aa610901267574673932
Lohith Bellad <lohith.pre@gmail.com>
2025-03-18 21:57:18 -0700

    Time: 63 ms (20.26%), Space: 20.5 MB (13.18%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        bracket = []
+        output = []
+        temp = []
+
+        for c in s:
+            if c == ")":
+                if len(bracket) > 0:
+                    if bracket[-1] == "(":
+                        bracket.pop()
+                        output.append(c)
+            elif c == "(":
+                bracket.append(c)
+                output.append(c)
+            else:
+                output.append(c)
+        
+        if len(bracket) == 0:
+            return "".join(output)
+
+        while len(bracket) > 0:
+            bracket.pop()
+            closed = 0
+            while len(output) > 0:
+                e = output.pop()
+                if e == ")":
+                    closed += 1
+                    temp.append(e)
+                elif e == "(":
+                    if closed > 0:
+                        closed -= 1
+                        temp.append(e)
+                else:
+                    temp.append(e)
+        
+        temp.reverse()
+        return "".join(temp)
+        
+    
