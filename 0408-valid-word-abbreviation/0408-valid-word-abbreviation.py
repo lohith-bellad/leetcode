0e94d721ccac7e156cfa24eb5cf349236ded205e
Lohith Bellad <lohith.pre@gmail.com>
2025-03-19 16:22:46 -0700

    Time: 0 ms (100%), Space: 17.6 MB (88.62%) - LeetHub

---
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -0,0 +1,23 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        abbr_ind = 0
+        word_ind = 0
+
+        while abbr_ind < len(abbr):
+            if abbr[abbr_ind] == "0" or word_ind >= len(word):
+                return False
+            
+            count = 0
+            while abbr_ind < len(abbr) and abbr[abbr_ind].isdigit():
+                count = count * 10 + int(abbr[abbr_ind])
+                abbr_ind += 1
+
+            if count > 0:
+                word_ind += count
+            else:
+                if word[word_ind] != abbr[abbr_ind]:
+                    return False
+                word_ind += 1
+                abbr_ind += 1
+        
+        return word_ind == len(word)
