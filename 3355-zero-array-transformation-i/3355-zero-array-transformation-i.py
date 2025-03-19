0a887e09f0f8979603fd22bcf12e8414599e7c9d
Lohith Bellad <lohith.pre@gmail.com>
2025-03-19 14:30:34 -0700

    Time: 108 ms (24.71%), Space: 55.5 MB (29.23%) - LeetHub

---
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        temp = [0 for i in range(len(nums) + 1)]
+
+        for r1, r2 in queries:
+            temp[r1] += 1
+            temp[r2 + 1] -= 1
+        
+        for i in range(1, len(temp)):
+            temp[i] += temp[i - 1]
+        
+        print(temp)
+        
+        for i in range(len(nums)):
+            if nums[i] > temp[i]:
+                return False
+        
+        return True
