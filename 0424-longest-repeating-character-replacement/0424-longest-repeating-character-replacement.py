0bdde539245b7962f1f286efec4bf63f2a25c9b1
Lohith Bellad <lohith.pre@gmail.com>
2025-03-21 15:01:12 -0700

    Time: 182 ms (26.89%), Space: 18 MB (48.56%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,20 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        table = {}
+        max_size = 1
+        left = 0
+
+        for right in range(len(s)):
+            if s[right] in table:
+                table[s[right]] += 1
+            else:
+                table[s[right]] = 1
+            
+            while (right - left + 1) - max(table.values()) > k:
+                table[s[left]] -= 1
+                left += 1
+            
+            max_size = max(max_size, right - left + 1)
+
+        return max_size
+        
