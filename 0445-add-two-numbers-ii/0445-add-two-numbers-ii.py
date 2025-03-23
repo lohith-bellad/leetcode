9c6aeb29f27f6f92161e9105ccc0d608611463b7
Lohith Bellad <lohith.pre@gmail.com>
2025-03-23 14:48:32 -0700

    Time: 12 ms (8.41%), Space: 18.1 MB (17.7%) - LeetHub

---
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -0,0 +1,52 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        def reverse(root: ListNode) -> ListNode:
+            prev= None
+
+            while root != None:
+                temp = root.next
+                root.next = prev
+                prev = root
+                root = temp
+            
+            return prev
+
+        l1_rev = reverse(l1)
+        l2_rev = reverse(l2)
+
+        dummy = ListNode(0)
+        head = dummy
+        carry = 0
+
+        while l1_rev != None and l2_rev != None:
+            s = l1_rev.val + l2_rev.val + carry
+            carry = s // 10
+            s = s % 10
+            head.next = ListNode(s, None)
+            head = head.next
+            l1_rev = l1_rev.next
+            l2_rev = l2_rev.next
+
+        l_rev = None        
+        if l1_rev != None:
+            l_rev = l1_rev
+        elif l2_rev != None:
+            l_rev = l2_rev
+
+        while l_rev != None:
+            s = l_rev.val + carry
+            carry = s // 10
+            s = s % 10
+            head.next = ListNode(s, None)
+            head = head.next
+            l_rev = l_rev.next
+
+        if carry > 0:
+            head.next = ListNode(carry, None)
+
+        return reverse(dummy.next)
