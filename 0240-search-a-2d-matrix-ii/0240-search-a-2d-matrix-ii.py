66db91f9fd06b5a8fe73cb3063dd874cdad0d7bb
Lohith Bellad <lohith.pre@gmail.com>
2025-03-25 15:16:41 -0700

    Time: 133 ms (96.06%), Space: 24.2 MB (22.02%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,11 +1,11 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         row_max = len(matrix) - 1
-        while matrix[row_max][0] > target:
+        while row_max >= 0 and matrix[row_max][0] > target:
             row_max -= 1
         
         col_max = len(matrix[0]) - 1
-        while matrix[0][col_max] > target:
+        while col_max >= 0 and matrix[0][col_max] > target:
             col_max -= 1
         
         for r in range(row_max + 1):
