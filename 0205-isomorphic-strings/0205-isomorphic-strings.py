ab1afea439ab2d1b6cb40acdf605e0efd2337857
Lohith Bellad <lohith.pre@gmail.com>
2025-03-25 11:28:37 -0700

    Time: 8 ms (29.1%), Space: 17.8 MB (80.24%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -10,7 +10,7 @@ class Solution:
             if s1 not in s_t_table and t1 not in t_s_table:
                 s_t_table[s1] = t1
                 t_s_table[t1] = s1
-            elif s_t_table[s1] != t1 or t_s_table[t1] != s1:
+            elif s_t_table.get(s1) != t1 or t_s_table.get(t1) != s1:
                 return False
         
         return True
