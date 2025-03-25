96870f96f6eef4a2424e7ab64e74a38efaa91222
Lohith Bellad <lohith.pre@gmail.com>
2025-03-24 21:50:29 -0700

    Time: 39 ms (60.33%), Space: 17.9 MB (37.13%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        is_negative = False
+        res = 0
+
+        if x < 0:
+            is_negative = True
+            inp = -x
+        else:
+            inp = x
+        
+        while inp > 0:
+            temp = (res * 10) + (inp % 10)
+            if temp > 2**31:
+                return 0
+            res = temp
+            inp = inp // 10
+
+        if is_negative:
+            return -res
+        else:
+            return res
