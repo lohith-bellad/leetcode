a99e82d3a9da45f51349e73104da7bdaded95a6c
Lohith Bellad <lohith.pre@gmail.com>
2025-03-25 14:58:53 -0700

    Time: 3 ms (18.92%), Space: 18 MB (29.12%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.py
@@ -0,0 +1,37 @@
+class Solution:
+    def gameOfLife(self, board: List[List[int]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        row_indices = [0, 0, 1, 1, 1, -1, -1, -1]
+        col_indices = [-1, 1, -1, 0, 1, -1, 0, 1]
+
+        row_max = len(board)
+        col_max = len(board[0])
+
+        for r in range(row_max):
+            for c in range(col_max):
+                count = 0
+
+                for i in range(len(row_indices)):
+                    new_row = r + row_indices[i]
+                    new_col = c + col_indices[i]
+
+                    if 0 <= new_row < row_max and 0 <= new_col < col_max:
+                        if board[new_row][new_col] > 0:
+                            count += 1
+                
+                if board[r][c] <= 0 and count == 3:
+                    board[r][c] = -1
+                elif board[r][c] > 0 and (count > 3 or count < 2):
+                    board[r][c] = 2
+
+
+        for r in range(row_max):
+            for c in range(col_max):
+                if board[r][c] == -1:
+                    board[r][c] = 1
+                elif board[r][c] == 2:
+                    board[r][c] = 0
+
+        return
