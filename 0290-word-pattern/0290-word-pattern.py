c26e173e332dbf47c26aff5c3fa1e3319751454e
Lohith Bellad <lohith.pre@gmail.com>
2025-03-25 11:39:33 -0700

    Time: 0 ms (100%), Space: 17.9 MB (16.25%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s_list = s.split(" ")
+        if len(s_list) != len(pattern):
+            return False
+
+        ps_table = {}
+        sp_table = {}
+    
+        for c, word in zip(pattern, s_list):
+            if (c not in ps_table) and (word not in sp_table):
+                ps_table[c] = word
+                sp_table[word] = c
+            
+            elif (ps_table.get(c) != word) or (sp_table.get(word) != c):
+                print(ps_table.get(c))
+                return False
+
+        return True
