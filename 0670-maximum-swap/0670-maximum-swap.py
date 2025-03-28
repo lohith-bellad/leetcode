c6f4b6b5835a69da294663c93248c8ede86e0f5a
Lohith Bellad <lohith.pre@gmail.com>
2025-03-28 13:15:56 -0700

    Time: 0 ms (100%), Space: 17.8 MB (56.95%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maximumSwap(self, num: int) -> int:
+        num_list = []
+        if num < 10:
+            return num
+
+        for c in str(num):
+            num_list.append(int(c))
+        
+        idx = 0
+        while idx < (len(num_list) - 1):
+            digit = num_list[idx]
+            if digit < max(num_list[idx + 1:]):
+                swap = max(num_list[idx + 1:])
+                for i in range(len(num_list) - 1, idx, -1):
+                    if num_list[i] == swap:
+                        num_list[idx], num_list[i] = num_list[i], num_list[idx]
+                        break
+                break
+            else:
+                idx += 1
+            
+        res = 0
+        for num in num_list:
+            res = (res * 10) + num
+
+        return res
