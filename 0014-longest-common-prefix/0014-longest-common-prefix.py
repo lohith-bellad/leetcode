6d8488c30eab3db44319844a229083ae1cf69893
Lohith Bellad <lohith.pre@gmail.com>
2025-03-29 22:29:43 -0700

    Time: 0 ms (100%), Space: 17.9 MB (48.68%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        inp = sorted(strs, key = lambda x: len(x))
+
+        output = inp[0]
+
+        for i in range(1, len(inp)):
+            ind = 0
+            while ind < len(output) and inp[i][ind] == output[ind]:
+                ind += 1
+            output = output[:ind]
+        
+        return output
