5dda07c0e7f842d5a01acaaf8da333c9fb50120e
Lohith Bellad <lohith.pre@gmail.com>
2025-03-30 22:50:32 -0700

    Time: 0 ms (100%), Space: 17.7 MB (91.2%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        def traverse(p: TreeNode, q: TreeNode) -> bool:
+            if p == None and q != None or p != None and q == None:
+                return False
+            
+            if p == None and q == None:
+                return True
+
+            if p.val != q.val:
+                return False
+            
+            left = traverse(p.left, q.left)
+            right = traverse(p.right, q.right)
+
+            return left and right
+        
+        return traverse(p, q)
