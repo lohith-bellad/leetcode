87088afcb014c3763a8065839b955485be43cfb2
Lohith Bellad <lohith.pre@gmail.com>
2025-04-05 00:33:02 -0700

    Time: 1 ms (53.14%), Space: 17.8 MB (76.49%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        row_max = len(matrix)
+        col_max = len(matrix[0])
+        map = {}
+        
+        for r in range(row_max):
+            for c in range(col_max):
+                if (r - c) not in map:
+                    map[r - c] = matrix[r][c]
+                elif map[r - c] != matrix[r][c]:
+                    return False
+        
+        return True
