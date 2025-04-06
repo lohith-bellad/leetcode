5fdbba631a11560b62871ff818984293343e04c3
Lohith Bellad <lohith.pre@gmail.com>
2025-04-05 17:24:56 -0700

    Time: 114 ms (21.91%), Space: 18.5 MB (35.65%) - LeetHub

---
+++ 0825-friends-of-appropriate-ages/0825-friends-of-appropriate-ages.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numFriendRequests(self, ages: List[int]) -> int:
+        age_cnt = [0 for i in range(121)]
+        requests = 0
+
+        for age in ages:
+            age_cnt[age] += 1
+        
+        for i, g1_cnt in enumerate(age_cnt):
+            for j, g2_cnt in enumerate(age_cnt):
+                if j <= (0.5 * i) + 7:
+                    continue
+                if j > i:
+                    continue
+                if j > 100 and i < 100:
+                    continue
+                if i == j:
+                    requests -= g1_cnt
+
+                requests += g1_cnt * g2_cnt
+
+        return requests
