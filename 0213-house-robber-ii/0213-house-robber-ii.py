be09f8a7d9fd917e501dc17aae60fc7c784d1ca8
Lohith Bellad <lohith.pre@gmail.com>
2025-04-06 14:12:12 -0700

    Time: 15 ms (1.22%), Space: 17.7 MB (85.86%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,35 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def find(nums: [], ind: int, memo: []) -> int:
+            if ind == len(nums) - 1 or ind == len(nums) - 2:
+                return nums[ind]
+
+            if memo[ind] != -1:
+                return memo[ind]
+
+            max_money = 0
+            i = ind + 2
+            while i < len(nums):
+                money = find(nums, i, memo)
+                max_money = max(max_money, money)
+                i += 1
+
+            memo[ind] = max_money + nums[ind]
+
+            return memo[ind]
+
+        def rob_path(nums: List[int], memo: []) -> int:
+            prev_sum = 0
+            cur_sum = 0
+
+            return max(find(nums, 0, memo), find(nums, 1, memo))
+        
+        memo1 = [-1 for i in range(len(nums) - 1)]
+        memo2 = [-1 for i in range(len(nums) - 1)]
+        if len(nums) == 1:
+            return nums[0]
+        
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+
+        return max(rob_path(nums[:-1], memo1), rob_path(nums[1:], memo2))
