2fbf86dc6d472b877d95cc2493e46ef3f7095544
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 16:55:29 -0700

    Time: 3224 ms (81.81%), Space: 17.9 MB (64.48%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,33 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+        
+        row_max = len(board)
+        col_max = len(board[0])
+
+        visited = [[0 for i in range(col_max)] for i in range(row_max)]
+
+        def dfs(r: int, c: int, ind: int) -> bool:
+            if ind == len(word):
+                return True
+            
+            res = False
+            for d in dirs:
+                nr = r + d[0]
+                nc = c + d[1]
+
+                if 0 <= nr < row_max and 0 <= nc < col_max and board[nr][nc] == word[ind] and visited[nr][nc] == 0:
+                    visited[nr][nc] = 1
+                    res = res or dfs(nr, nc, ind + 1)
+                    visited[nr][nc] = 0
+            return res
+
+        for i in range(row_max):
+            for j in range(col_max):
+                if board[i][j] == word[0]:
+                    visited[i][j] = 1
+                    if dfs(i, j, 1) == True:
+                        return True
+                    visited[i][j] = 0
+        
+        return False
