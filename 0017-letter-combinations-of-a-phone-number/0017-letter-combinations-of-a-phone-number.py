f44cb9c53039d357f65624e63b1117a21d7363d0
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 14:11:05 -0700

    Time: 0 ms (100%), Space: 17.8 MB (41.85%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,54 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        """
+        output = []
+        queue = deque()
+
+        phoneBook = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+
+        for d in digits:
+            num = int(d)
+
+            key = phoneBook[num]
+            if len(queue) == 0:
+                for i in range(len(key)):
+                    if len(digits) == 1:
+                        output.append(key[i])
+                    else:
+                        queue.append(key[i])
+                continue
+
+            size = len(queue)
+            while size > 0:
+                old = queue.popleft()
+                for k in key:
+                    new = old + k
+                    if len(new) == len(digits):
+                        output.append(new)
+                    else:
+                        queue.append(new)
+                size -= 1
+                    
+        return output
+        """
+        phoneBook = {2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno", 7: "pqrs", 8: "tuv", 9: "wxyz"}
+
+        def traverse(ind: int, cur_word: str):
+            if len(cur_word) == len(digits):
+                output.append(cur_word)
+                return
+            
+            for c in phoneBook[int(digits[ind])]:
+                traverse(ind + 1, cur_word + c)
+            
+            return
+
+        output = []
+
+        if len(digits) > 0:
+            traverse(0, "")
+        
+        return output
+        
+
+            
