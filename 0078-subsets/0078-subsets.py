6196ef7d93c731b1b7a4eab53589b39ca9a636e6
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 11:03:08 -0700

    Time: 0 ms (100%), Space: 18.1 MB (21.47%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        def traverse(nums: List[int], ind: int, cur_subset: [], output: []):
+            if ind == len(nums):
+                output.append(cur_subset.copy())
+                return
+            
+            cur_subset.append(nums[ind])
+            traverse(nums, ind + 1, cur_subset, output)
+            cur_subset.pop()
+
+            traverse(nums, ind + 1, cur_subset, output)
+            return
+
+        cur_subset = []
+        output = []
+        traverse(nums, 0, cur_subset, output)
+        return output
