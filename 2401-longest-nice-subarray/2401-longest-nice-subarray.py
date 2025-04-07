e49f222d0d76b08ec7be7b193e19abf5ef872645
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 16:01:47 -0700

    Time: 81 ms (90.46%), Space: 32.9 MB (47.82%) - LeetHub

---
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestNiceSubarray(self, nums: List[int]) -> int:
+        if len(nums) < 2:
+            return 1
+        
+        left = 0
+        right = 1
+        max_size = 1
+
+        while right < len(nums):
+            i = left
+            while i < right:
+                if nums[i] & nums[right] != 0:
+                    break
+                else:
+                    i += 1
+            max_size = max(max_size, i - left + 1)
+            if i == right:
+                right += 1
+            else:
+                left = i + 1
+                right = left + 1
+        
+        return max_size
+            
