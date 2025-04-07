7e4d11074130c3c7aa599475c2530c8dd1ead67d
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 14:52:25 -0700

    Time: 45 ms (6.56%), Space: 17.8 MB (84.16%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        def traverse(candidates: List[int], target: int, ind: int, combs: [], output: []):
+            if sum(combs) == target:
+                output.append(combs.copy())
+                return
+            
+            if ind > len(candidates) - 1:
+                return
+            
+            if sum(combs) > target:
+                return
+                
+            combs.append(candidates[ind])
+            traverse(candidates, target, ind + 1, combs, output)
+            combs.pop()
+
+            while ind + 1 < len(candidates) and candidates[ind] == candidates[ind + 1]:
+                ind += 1
+            traverse(candidates, target, ind + 1, combs, output)
+            return
+
+        output = []
+
+        candidates.sort()
+        traverse(candidates, target, 0, [], output)
+        return output
