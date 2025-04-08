8ba8eb58362920e75c02c2b9ce3a95dbe5fc542f
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 22:50:30 -0700

    Time: 3 ms (87.34%), Space: 18.8 MB (63.19%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        def traverse(s: str, ind: int) -> [""]:
+            if ind == len(s):
+                return [""]
+            
+            perms = traverse(s, ind + 1)
+            temp = []
+            for perm in perms:
+                if s[ind].isalpha():
+                    clower = s[ind].lower()
+                    cupper = s[ind].upper()
+                    temp.append(clower + perm)
+                    temp.append(cupper + perm)
+                else:
+                    temp.append(s[ind] + perm)
+            return temp
+        
+        return traverse(s, 0)
