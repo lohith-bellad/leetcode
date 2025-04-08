0889a078c64adfeb66f7a766cf41f97144cdf3b0
Lohith Bellad <lohith.pre@gmail.com>
2025-04-07 22:36:10 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        def traverse(nums: List[int], ind: int) -> [[]]:
+            if ind == len(nums):
+                return [[]]
+            
+            temp = []
+            thash = {}
+            perms = traverse(nums, ind + 1)
+            for perm in perms:
+                for j in range(len(perm) + 1):
+                    pc = perm.copy()
+                    pc.insert(j, nums[ind])
+                    key = int(''.join(map(str, pc)))
+                    if key not in thash:
+                        temp.append(pc)
+                        thash[key] = 1
+            return list(temp)
+        
+        return traverse(nums, 0)
