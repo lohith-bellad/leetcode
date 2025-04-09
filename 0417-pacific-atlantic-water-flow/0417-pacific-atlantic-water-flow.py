d37bd5993ce8c328da8f22d2ae0ec12624f57ea7
Lohith Bellad <lohith.pre@gmail.com>
2025-04-09 12:21:28 -0700

    Time: 58 ms (19.15%), Space: 19.2 MB (79.97%) - LeetHub

---
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -0,0 +1,37 @@
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        def traverse(queue, row_max, col_max):
+            dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+            output = set()
+            while len(queue) > 0:
+                r, c = queue.popleft()
+                output.add((r, c))
+
+                for d in dirs:
+                    nr = r + d[0]
+                    nc = c + d[1]
+
+                    if 0 <= nr < row_max and 0 <= nc < col_max and (nr, nc) not in output and heights[nr][nc] >= heights[r][c]:
+                        queue.append((nr, nc))
+
+            return output
+
+        row_max = len(heights)
+        col_max = len(heights[0])
+
+        p_queue = deque()
+        a_queue = deque()
+
+        for i in range(row_max):
+            p_queue.append((i, 0))
+            a_queue.append((i, col_max - 1))
+        
+        for i in range(col_max):
+            p_queue.append((0, i))
+            a_queue.append((row_max - 1, i))
+        
+        p_regions = traverse(p_queue, row_max, col_max)
+        a_regions = traverse(a_queue, row_max, col_max)
+
+        return list(p_regions & a_regions)
