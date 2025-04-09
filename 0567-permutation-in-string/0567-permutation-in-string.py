ca58217b6465289c14e7bd6ca224064375efa0bf
Lohith Bellad <lohith.pre@gmail.com>
2025-04-08 22:01:00 -0700

    Time: 16 ms (73.92%), Space: 17.9 MB (40.46%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -0,0 +1,34 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2):
+            return False
+            
+        s1_map = {}
+        s2_map = {}
+
+        for c in s1:
+            s1_map[c] = s1_map.get(c, 0) + 1
+        
+        for i in range(len(s1)):
+            s2_map[s2[i]] = s2_map.get(s2[i], 0) + 1
+
+        if s2_map == s1_map:
+            return True
+        
+        start = 0
+        end = len(s1)
+
+        while end < len(s2):
+            s2_map[s2[start]] -= 1
+            if s2_map[s2[start]] == 0:
+                del s2_map[s2[start]]
+            
+            s2_map[s2[end]] = s2_map.get(s2[end], 0) + 1
+
+            if s2_map == s1_map:
+                return True
+            
+            end += 1
+            start += 1
+        
+        return False
