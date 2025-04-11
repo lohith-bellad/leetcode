fa18444a2a1ec53fe95e9b8957c1b1e3d51daa10
Lohith Bellad <lohith.pre@gmail.com>
2025-04-11 15:39:12 -0700

    Time: 499 ms (47.45%), Space: 43.7 MB (22.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        """
         def traverse(p1: int, p2: int, cache:[]) -> int:
             if p1 >= len(text1) or p2 >= len(text2):
                 return 0
@@ -16,4 +17,16 @@ class Solution:
             return cache[p1][p2]
         
         cache = [[-1 for i in range(len(text2))] for i in range(len(text1))]
-        return traverse(0, 0, cache)       
+        return traverse(0, 0, cache)
+        """
+
+        cache = [[0 for i in range(len(text2) + 1)] for i in range(len(text1) + 1)]
+
+        for i in range(len(text1)):
+            for j in range(len(text2)):
+                if text1[i] == text2[j]:
+                    cache[i + 1][j + 1] = 1 + cache[i][j]
+                else:
+                    cache[i + 1][j + 1] = max(cache[i][j + 1], cache[i + 1][j])
+        
+        return cache[-1][-1]
