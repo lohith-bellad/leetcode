a436e76339e6b4744eeb20c29f68ed79c87c17d7
Lohith Bellad <lohith.pre@gmail.com>
2025-04-11 16:17:23 -0700

    Time: 30 ms (99.06%), Space: 18.6 MB (23.57%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        def traverse(p1, p2, p3):
+            if p3 == len(s3):
+               return True
+            
+            if (p1, p2) in mapping:
+                return mapping[(p1, p2)]
+
+            res = False
+            if p1 < len(s1) and s1[p1] == s3[p3]:
+                res = traverse(p1 + 1, p2, p3 + 1)
+            
+            if res == False and p2 < len(s2) and s2[p2] == s3[p3]:
+                res = traverse(p1, p2 + 1, p3 + 1)
+            
+            mapping[(p1, p2)] = res
+            
+            return res
+
+
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        mapping = {}
+        return traverse(0, 0, 0)
