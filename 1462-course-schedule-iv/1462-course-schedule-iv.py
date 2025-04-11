5675092f8047b0a05fd5e5fa577e7b82a6fc7a1d
Lohith Bellad <lohith.pre@gmail.com>
2025-04-11 12:28:10 -0700

    Time: 88 ms (43.87%), Space: 20.4 MB (63.35%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.py
+++ 1462-course-schedule-iv/1462-course-schedule-iv.py
@@ -1,19 +1,17 @@
 class Solution:
     def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
-        def dfs(course, visited, hash_map, order):
+        def dfs(course, visited, hash_map):
             if course in visited:
                 return
             
             visited.add(course)
 
             if course not in hash_map:
-                order.append(course)
                 return
             
             for prereqs in hash_map[course]:
-                dfs(prereqs, visited, hash_map, order)
+                dfs(prereqs, visited, hash_map)
             
-            order.append(course)
             return
 
         hash_map = defaultdict(list)
@@ -21,26 +19,19 @@ class Solution:
         for prereqs in prerequisites:
             hash_map[prereqs[1]].append(prereqs[0])
         
-        visited = set()
         order = []
+        pre_map = {}
 
         for course in range(numCourses):
-            temp = []
-            dfs(course, visited, hash_map, temp)
-            order.append(temp)
+            visited = set()
+            dfs(course, visited, hash_map)
+            pre_map[course] = visited
         
         output = []
-        
-        for course_a, course_b in queries:
-            for i in range(len(order)):
-                if course_b in order[i]:
-                    m_ind = i
-                    break
-            
-            ind_b = order[m_ind].index(course_b)
-            if course_a in order[m_ind][:ind_b]:
+        for prereq, course in queries:
+            if prereq in pre_map[course]:
                 output.append(True)
             else:
                 output.append(False)
-        
+
         return output
