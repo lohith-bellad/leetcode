f53295d79f69fc1a922875a67a2411f094f7c3dd
Lohith Bellad <lohith.pre@gmail.com>
2025-04-11 16:23:34 -0700

    Time: 36 ms (92.54%), Space: 18.7 MB (14.7%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -7,15 +7,16 @@ class Solution:
             if (p1, p2) in mapping:
                 return mapping[(p1, p2)]
 
-            res = False
-            if p1 < len(s1) and s1[p1] == s3[p3]:
+            if p1 < len(s1) and p2 < len(s2) and s1[p1] == s2[p2] and s1[p1] == s3[p3]:
+                res = traverse(p1 + 1, p2, p3 + 1) or traverse(p1, p2 + 1, p3 + 1)
+            elif p1 < len(s1) and s1[p1] == s3[p3]:
                 res = traverse(p1 + 1, p2, p3 + 1)
-            
-            if res == False and p2 < len(s2) and s2[p2] == s3[p3]:
+            elif p2 < len(s2) and s2[p2] == s3[p3]:
                 res = traverse(p1, p2 + 1, p3 + 1)
+            else:
+                res = False
             
             mapping[(p1, p2)] = res
-            
             return res
 
 
