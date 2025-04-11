0320d03ea83b6753dfe5184299baeed230c04ac0
Lohith Bellad <lohith.pre@gmail.com>
2025-04-11 16:34:22 -0700

    Time: 1500 ms (24.6%), Space: 43.4 MB (36.96%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,29 +1,14 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         size = len(s)
-        cache = [[-1 for i in range(size)] for i in range(size)]
+        cache = [[0 for i in range(size + 1)] for i in range(size + 1)]
 
-        def dfs(i: int, j: int) -> int:
-            if i < 0 or j >= size:
-                return 0
-            
-            if cache[i][j] != -1:
-                return cache[i][j]
-
-            if s[i] == s[j]:
-                l = 1 if i == j else 2
-                cache[i][j] = l + dfs(i - 1, j + 1)
-            else:
-                cache[i][j] = max(dfs(i - 1, j), dfs(i, j + 1))
-
-            return cache[i][j]
-
+        sr = s[::-1]
         for i in range(size):
-            dfs(i, i)
-            dfs(i, i + 1)
-        
-        temp = []
-        for r in range(size):
-            temp.append(max(cache[r]))
-        
-        return max(temp)
+            for j in range(size):
+                if s[i] == sr[j]:
+                    cache[i + 1][j + 1] = 1 + cache[i][j]
+                else:
+                    cache[i + 1][j + 1] = max(cache[i + 1][j], cache[i][j + 1])
+
+        return cache[-1][-1] 
