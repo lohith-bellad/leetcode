511885863f3576387b3b5b92507c120fef8f2964
Lohith Bellad <lohith.pre@gmail.com>
2025-04-10 19:18:27 -0700

    Time: 5479 ms (7.05%), Space: 23.1 MB (5.09%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        def traverse(ind: int) -> bool:
+            if cache[ind] != -1:
+                return cache[ind]
+
+            min_jumps = float('inf')
+            for i in range(ind + 1, min(len(nums) - 1, ind + nums[ind]) + 1):
+                jumps = traverse(i) + 1
+                min_jumps = min(min_jumps, jumps)
+            
+            cache[ind] = min_jumps
+            return cache[ind]
+        
+        cache = [-1 for i in range(len(nums))]
+        cache[-1] = 0
+        return traverse(0)
