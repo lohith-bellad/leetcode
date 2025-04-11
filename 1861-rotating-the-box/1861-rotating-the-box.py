fbbd05a8152f7ed0c1215f22f6cba5c5e095aabd
Lohith Bellad <lohith.pre@gmail.com>
2025-04-10 23:24:30 -0700

    Time: 182 ms (34.62%), Space: 63.4 MB (42.18%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.py
@@ -0,0 +1,27 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        row_max = len(boxGrid)
+        col_max = len(boxGrid[0])
+
+        output = [["" for i in range(row_max)] for i in range(col_max)]
+
+        for i in range(col_max):
+            for j in range(row_max):
+                output[i][j] = boxGrid[j][i]
+
+        for i in range(col_max):
+            output[i].reverse()
+ 
+        for i in range(row_max):
+            bottom_most = col_max - 1
+            for ind in range(col_max - 1, -1, -1):
+                if output[ind][i] == "#":
+                    output[ind][i] = "."
+                    output[bottom_most][i] = "#"
+                    bottom_most -= 1
+
+                if output[ind][i] == "*":
+                    bottom_most = ind - 1
+
+        return output
+        
