166dfb7502cab7b763d9429d455754d7d9d564ea
Lohith Bellad <lohith.pre@gmail.com>
2025-04-12 13:50:34 -0700

    Time: 475 ms (14.58%), Space: 28.5 MB (76.64%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        start = 1
+        end = max(nums)
+
+        while start < end:
+            mid = start + (end - start) // 2
+
+            rob = 0
+            ind = 0
+            while ind < len(nums):
+                if nums[ind] <= mid:
+                    rob += 1
+                    ind += 2
+                else:
+                    ind += 1
+            
+            if rob >= k:
+                end = mid
+            else:
+                start = mid + 1
+
+        return start
