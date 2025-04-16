6ec7c00a9960dfcdbf0ffa0994bdfa2c3c7bb479
Lohith Bellad <lohith.pre@gmail.com>
2025-04-16 12:25:15 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -0,0 +1,27 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        if s == "":
+            return 0
+    
+        cal_stack = []
+        s = s + "+"
+        num = 0
+        res = 0
+        op = "+"
+
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+            elif c in "+/*-":
+                if op == "+":
+                    cal_stack.append(num)
+                elif op == "-":
+                    cal_stack.append(-num)
+                elif op == "*":
+                    cal_stack.append(cal_stack.pop() * num)
+                else:
+                    cal_stack.append(cal_stack.pop() // num)
+                num = 0
+                op = c
+    
+        return sum(cal_stack)
