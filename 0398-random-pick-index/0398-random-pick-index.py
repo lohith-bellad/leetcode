277183561f2cf7795e921715a8a039883c29ed2b
Lohith Bellad <lohith.pre@gmail.com>
2025-04-17 13:52:54 -0700

    Time: 60 ms (11.78%), Space: 28 MB (78.67%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.map = defaultdict(list)
+
+        for i, val in enumerate(nums):
+            self.map[val].append(i)
+
+    def pick(self, target: int) -> int: 
+        idx = random.randint(0, len(self.map[target]) - 1)
+        return self.map[target][idx]
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
