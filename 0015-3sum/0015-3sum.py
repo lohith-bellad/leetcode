af17113b569644830cadb8b46e542fec287dc677
Lohith Bellad <lohith.pre@gmail.com>
2025-04-19 15:22:46 -0700

    Time: 519 ms (78.13%), Space: 20.8 MB (23.19%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,34 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        output = set()
         left_hash = {}
         right_hash = {}
-        output = set()
-
         nums.sort()
+    
         left_hash[nums[0]] = 1
         for i in range(1, len(nums)):
-            if nums[i] not in right_hash:
-                right_hash[nums[i]] = 0
-            right_hash[nums[i]] += 1
-
-        idx = 1
-        while idx < len(nums) - 1:
-            if nums[idx] in right_hash:
-                right_hash[nums[idx]] -= 1
-                if right_hash[nums[idx]] == 0:
-                    del right_hash[nums[idx]]
-            
-            for n in left_hash.keys():
-                target = -(n + nums[idx])
-                if target in right_hash:
-                    output.add((n, nums[idx], target))
-            
-            if nums[idx] in left_hash:
-                left_hash[nums[idx]] += 1
-            else:
-                left_hash[nums[idx]] = 1
-
-            idx += 1
+            right_hash[nums[i]] = right_hash.get(nums[i], 0) + 1
         
+        for i in range(1, len(nums)):
+            right_hash[nums[i]] -= 1
+            if right_hash[nums[i]] == 0:
+                del right_hash[nums[i]]
+            
+            for key in left_hash.keys():
+                search = -(key + nums[i])
+                if search in right_hash:
+                    output.add((key, nums[i], search))
+            
+            left_hash[nums[i]] = left_hash.get(nums[i], 0) + 1
+    
         return list(output)
+
 
