b5022ae5761aff7c51460880dc950f5949f1c87e
Lohith Bellad <lohith.pre@gmail.com>
2025-04-20 11:47:36 -0700

    Time: 0 ms (100%), Space: 17.9 MB (18.51%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -1,47 +1,23 @@
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
-        abbr_ptr = 0
         word_ptr = 0
-
-        """
-        while abbr_ind < len(abbr):
-            if abbr[abbr_ind] == "0" or word_ind >= len(word):
+        abbr_ptr = 0
+    
+        while abbr_ptr < len(abbr):
+            if abbr[abbr_ptr] == "0" or word_ptr >= len(word):
                 return False
-            
-            count = 0
-            while abbr_ind < len(abbr) and abbr[abbr_ind].isdigit():
-                count = count * 10 + int(abbr[abbr_ind])
-                abbr_ind += 1
-
-            if count > 0:
-                word_ind += count
-            else:
-                if word[word_ind] != abbr[abbr_ind]:
-                    return False
-                word_ind += 1
-                abbr_ind += 1
-        
-        return word_ind == len(word)
-        """
-        while word_ptr < len(word) and abbr_ptr < len(abbr):
-            if abbr[abbr_ptr] == "0":
-                return False
-
+                
             if abbr[abbr_ptr].isdigit():
                 num = 0
                 while abbr_ptr < len(abbr) and abbr[abbr_ptr].isdigit():
                     num = num * 10 + int(abbr[abbr_ptr])
-                    abbr_ptr += 1
-            
+                    abbr_ptr += 1            
                 word_ptr += num
-                if word_ptr >= len(word) and abbr_ptr < len(abbr):
-                    return False
-        
             else:
                 if word[word_ptr] != abbr[abbr_ptr]:
                     return False
-            
+                
                 word_ptr += 1
                 abbr_ptr += 1
-
-        return word_ptr == len(word) and abbr_ptr == len(abbr)
+    
+        return word_ptr == len(word)
