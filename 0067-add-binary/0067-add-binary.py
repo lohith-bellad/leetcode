9de7e3012c9db7803221a4791a3fc0e1ff7efce2
Lohith Bellad <lohith.pre@gmail.com>
2025-04-20 22:22:15 -0700

    Time: 2 ms (49.9%), Space: 17.7 MB (91.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,68 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        """
-        result = ""
-        carry = 0
-        i = len(a) - 1
-        j = len(b) - 1
-
-        while i >= 0 and j >= 0:
-            if a[i] == "1" and b[j] == "1":
-                if carry == 0:
-                    result += '0'
-                else:
-                    result += '1'
-                carry = 1
-            elif (a[i] == "1" and b[j] == "0") or (a[i] == "0" and b[j] == "1"):
-                if carry == 0:
-                    result += '1'
-                else:
-                    result += '0'
-            else:
-                if carry == 0:
-                    result += '0'
-                else:
-                    result += '1'
-                    carry = 0
-            i -= 1
-            j -= 1
-        
-        if len(a) > len(b):
-            print(i)
-            while i >= 0:
-                if a[i] == "1":
-                    if carry == 0:
-                        result += '1'
-                    else:
-                        result += '0'
-                else:
-                    if carry == 0:
-                        result += '0'
-                    else:
-                        result += '1'
-                        carry = 0
-                i -= 1
-        
-        if len(a) < len(b):
-            while j >= 0:
-                if b[j] == "1":
-                    if carry == 0:
-                        result += '1'
-                    else:
-                        result += '0'
-                else:
-                    if carry == 0:
-                        result += '0'
-                    else:
-                        result += '1'
-                        carry = 0
-                j -= 1
        
-        if carry == 1:
-            result += '1'
-
-        return result[::-1]
-        """
         
         p1 = len(a) - 1
         p2 = len(b) - 1
@@ -88,6 +26,7 @@ class Solution:
                     result += "0"
                 else:
                     result += "1"
+                    carry = 0
             else:
                 if carry:
                     result += "1"
