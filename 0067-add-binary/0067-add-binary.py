b93f9d3fbdbf13ff35bce487486d913764844b85
Lohith Bellad <lohith.pre@gmail.com>
2025-04-20 22:21:39 -0700

    Time: 4 ms (18.41%), Space: 17.9 MB (28.36%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,103 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        """
+        result = ""
+        carry = 0
+        i = len(a) - 1
+        j = len(b) - 1
+
+        while i >= 0 and j >= 0:
+            if a[i] == "1" and b[j] == "1":
+                if carry == 0:
+                    result += '0'
+                else:
+                    result += '1'
+                carry = 1
+            elif (a[i] == "1" and b[j] == "0") or (a[i] == "0" and b[j] == "1"):
+                if carry == 0:
+                    result += '1'
+                else:
+                    result += '0'
+            else:
+                if carry == 0:
+                    result += '0'
+                else:
+                    result += '1'
+                    carry = 0
+            i -= 1
+            j -= 1
+        
+        if len(a) > len(b):
+            print(i)
+            while i >= 0:
+                if a[i] == "1":
+                    if carry == 0:
+                        result += '1'
+                    else:
+                        result += '0'
+                else:
+                    if carry == 0:
+                        result += '0'
+                    else:
+                        result += '1'
+                        carry = 0
+                i -= 1
+        
+        if len(a) < len(b):
+            while j >= 0:
+                if b[j] == "1":
+                    if carry == 0:
+                        result += '1'
+                    else:
+                        result += '0'
+                else:
+                    if carry == 0:
+                        result += '0'
+                    else:
+                        result += '1'
+                        carry = 0
+                j -= 1
+       
+        if carry == 1:
+            result += '1'
+
+        return result[::-1]
+        """
+        
+        p1 = len(a) - 1
+        p2 = len(b) - 1
+        carry = 0
+        result = ""
+
+        while p1 >=0 or p2 >= 0:
+            n1 = 0
+            n2 = 0
+            if p1 >= 0:
+                n1 = int(a[p1])
+            if p2 >= 0:
+                n2 = int(b[p2])
+
+            if n1 == 1 and n2 == 1:
+                if carry:
+                    result += "1"
+                else:
+                    result += "0"
+                carry = 1
+            elif n1 ^ n2:
+                if carry:
+                    result += "0"
+                else:
+                    result += "1"
+            else:
+                if carry:
+                    result += "1"
+                    carry = 0
+                else:
+                    result += "0"
+            p1 -= 1
+            p2 -= 1
+
+        if carry:
+            result += "1"
+
+        return result[::-1]
