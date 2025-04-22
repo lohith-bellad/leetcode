cf6f6eddf4feda31b16d48b8f3fa65125fdc47e8
Lohith Bellad <lohith.pre@gmail.com>
2025-04-22 07:27:14 -0700

    Time: 19 ms (85.17%), Space: 19 MB (26.82%) - LeetHub

---
+++ 0490-the-maze/0490-the-maze.py
@@ -0,0 +1,33 @@
+class Solution:
+    def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
+        row_max = len(maze)
+        col_max = len(maze[0])
+
+        dirs = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+        def dfs(row: int, col: int, visited: {}) -> bool:
+            if (row, col) in visited:
+                return False
+
+            if row == destination[0] and col == destination[1]:
+                return True
+
+            visited.add((row, col))
+
+            for d in dirs:
+                nrow = row
+                ncol = col
+
+                while 0 <= nrow < row_max and 0 <= ncol < col_max and maze[nrow][ncol] == 0:
+                    nrow += d[0]
+                    ncol += d[1]
+                nrow -= d[0]
+                ncol -= d[1]
+
+                if dfs(nrow, ncol, visited):
+                    return True
+
+            return False
+
+        visited = set()
+        return dfs(start[0], start[1], visited)
