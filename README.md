c24f82135ef35b3ad50001cd4276b2ad56e9e040
lohith bellad <lohith.pre@gmail.com>
2025-11-11 20:55:27 -0800

    Update

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
 # LeetCode Problems
 
-**Total Problems: 5** | Easy: 1 | Medium: 4 | Hard: 0
+**Total Problems: 10** | Easy: 3 | Medium: 7 | Hard: 0
 
 | Serial # | Problem # | Problem Title | Difficulty |
 |----------|-----------|---------------|------------|
-| 1 | 215 | Kth Largest Element in an Array | Medium |
-| 2 | 314 | Binary Tree Vertical Order Traversal | Medium |
-| 3 | 408 | Valid Word Abbreviation | Easy |
-| 4 | 680 | Valid Palindrome II | Medium |
-| 5 | 1249 | Minimum Remove to Make Valid Parentheses | Medium |
+| 1 | 88 | Merge Sorted Array | Easy |
+| 2 | 199 | Binary Tree Right Side View | Medium |
+| 3 | 215 | Kth Largest Element in an Array | Medium |
+| 4 | 314 | Binary Tree Vertical Order Traversal | Medium |
+| 5 | 408 | Valid Word Abbreviation | Easy |
+| 6 | 543 | Diameter of Binary Tree | Easy |
+| 7 | 680 | Valid Palindrome II | Medium |
+| 8 | 1249 | Minimum Remove to Make Valid Parentheses | Medium |
+| 9 | 1650 | Lowest Common Ancestor of a Binary Tree III | Medium |
+| 10 | 1762 | Buildings With an Ocean View | Medium |
---
+++ problems/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -0,0 +1,82 @@
+"""
+1650. Lowest Common Ancestor of a Binary Tree III
+Medium
+
+Given two nodes of a binary tree p and q, return their lowest common ancestor (LCA).
+
+Each node will have a reference to its parent node. The definition for Node is below:
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
+
+According to the definition of LCA on Wikipedia: "The lowest common ancestor of two nodes p and q
+in a tree T is the lowest node that has both p and q as descendants (where we allow a node to be
+a descendant of itself)."
+
+
+Example 1:
+    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+    Output: 3
+    Explanation: The LCA of nodes 5 and 1 is 3.
+
+Example 2:
+    Input: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+    Output: 5
+    Explanation: The LCA of nodes 5 and 4 is 5 since a node can be a descendant of itself according to the LCA definition.
+
+Example 3:
+    Input: root = [1,2], p = 1, q = 2
+    Output: 1
+
+
+Constraints:
+    * The number of nodes in the tree is in the range [2, 10^5]
+    * -10^9 <= Node.val <= 10^9
+    * All Node.val are unique
+    * p != q
+    * p and q exist in the tree
+
+"""
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+
+class Solution:
+    def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
+        p_height = 0
+        q_height = 0
+        ptemp = p
+        qtemp = q
+
+        while ptemp.parent != None:
+            p_height += 1
+            ptemp = ptemp.parent
+
+        while qtemp.parent != None:
+            q_height += 1
+            qtemp = qtemp.parent
+
+        ptemp = p
+        while p_height > q_height:
+            ptemp = ptemp.parent
+            p_height -= 1
+
+        qtemp = q
+        while q_height > p_height:
+            qtemp = qtemp.parent
+            q_height -= 1
+
+        while ptemp != qtemp:
+            ptemp = ptemp.parent
+            qtemp = qtemp.parent
+
+        return ptemp
