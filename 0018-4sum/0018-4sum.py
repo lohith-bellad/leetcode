67295a214ae900fad3c9f423879f90eb8047a2a6
Lohith Bellad <lohith.pre@gmail.com>
2026-05-01 15:45:31 -0700

    Time: 455 ms (20.92%), Space: 19.3 MB (85.8%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        """
         hashset = set()
         output = set()
 
@@ -12,3 +13,35 @@ class Solution:
             hashset.add(nums[i])
         
         return list(output)
+        """
+        output = []
+        nums.sort()
+
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, len(nums)):
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+
+                left = j + 1
+                right = len(nums) - 1
+
+                while left < right:
+                    pivot = target - nums[i] - nums[j]
+
+                    if nums[left] + nums[right] < pivot:
+                        left += 1
+                    elif nums[left] + nums[right] > pivot:
+                        right -= 1
+                    else:
+                        output.append([nums[i], nums[j], nums[left], nums[right]])
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                    
+                        left += 1
+                        right -= 1
+        
+        return output
