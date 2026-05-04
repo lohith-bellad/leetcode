be8f2437ee7311f0b9e5b52316374e5262921baa
Lohith Bellad <lohith.pre@gmail.com>
2026-05-03 17:10:22 -0700

    Time: 155 ms (21.73%), Space: 25 MB (47.12%) - LeetHub

---
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numberOfSubarrays(self, nums: List[int], k: int) -> int:
+        def at_most(k):
+            odd_count = 0
+            left = 0
+            count = 0
+
+            for right in range(len(nums)):
+                if nums[right] % 2 == 1:
+                    odd_count += 1
+                
+                while odd_count > k:
+                    if nums[left] % 2 == 1:
+                        odd_count -= 1
+                    left += 1
+                
+                count += right - left + 1
+
+            return count
+        
+        return at_most(k) - at_most(k - 1) 
