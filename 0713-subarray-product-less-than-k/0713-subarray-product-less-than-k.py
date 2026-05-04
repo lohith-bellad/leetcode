c2f6af828aaf24b95809c0c92c209cc49c886a69
Lohith Bellad <lohith.pre@gmail.com>
2026-05-03 17:21:43 -0700

    Time: 56 ms (72.1%), Space: 22.4 MB (14.93%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+        if k <= 1:
+            return 0
+
+        left = 0
+        cur_product = 1
+        count = 0
+
+        for right in range(len(nums)):
+            cur_product *= nums[right]
+
+            while left <= right and cur_product >= k:
+                cur_product //= nums[left]
+                left += 1
+            
+            count += right - left + 1
+        
+        return count
