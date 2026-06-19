01529477223e8703f9e215d1a70ccc43c2024bb9
Lohith Bellad <lohith.pre@gmail.com>
2026-06-19 12:45:05 -0700

    Time: 3 ms (91.42%), Space: 19.2 MB (81.69%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        """
+        if len(s) != len(t):
+            return False
+
+        s_t_table = {}
+        t_s_table = {}
+
+        for s1, t1 in zip(s, t):
+            if s1 not in s_t_table and t1 not in t_s_table:
+                s_t_table[s1] = t1
+                t_s_table[t1] = s1
+            elif s_t_table.get(s1) != t1 or t_s_table.get(t1) != s1:
+                return False
+        
+        return True
+        """
+        sTable = {}
+        tTable = {}
+
+        for s1, t1 in zip(s, t):
+            if s1 not in sTable and t1 not in tTable:
+                sTable[s1] = t1
+                tTable[t1] = s1
+            elif sTable.get(s1) != t1 or tTable.get(t1) != s1:
+                return False
+        
+        return True
