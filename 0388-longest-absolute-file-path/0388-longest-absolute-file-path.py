b82468f47ba6606211e202342d02fb0e787bb569
Lohith Bellad <lohith.pre@gmail.com>
2026-06-20 08:15:24 -0700

    Time: 0 ms (100%), Space: 19.4 MB (25.14%) - LeetHub

---
+++ 0388-longest-absolute-file-path/0388-longest-absolute-file-path.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthLongestPath(self, input: str) -> int:
+        myStack = []
+        if "." not in input:
+            return 0
+
+        inps = input.split("\n")
+        max_len = 0
+        
+        for inp in inps:
+            level = inp.count("\t")
+            name_len = len(inp) - level
+            
+            if not myStack:
+                myStack.append((level, name_len))
+            else:
+                while myStack and myStack[-1][0] >= level:
+                    myStack.pop()
+                
+                if myStack:
+                    last = myStack[-1][1]
+                else:
+                    last = 0
+                myStack.append((level, name_len + last))
+            
+            if "." in inp:
+                max_len = max(max_len, sum(myStack[-1]))
+
+        return max_len
