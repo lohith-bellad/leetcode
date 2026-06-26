978acd7de042eb4e3f592f49da6cc55440393c68
Lohith Bellad <lohith.pre@gmail.com>
2026-06-26 13:30:59 -0700

    Time: 178 ms (5.31%), Space: 19.4 MB (27.21%) - LeetHub

---
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.py
@@ -0,0 +1,31 @@
+class Solution:
+    def numSteps(self, s: str) -> int:
+        count = 0
+        s = list(s)
+
+        while '1' in s[:len(s) - 1]:
+            pivot = s[-1]
+            
+            #Even number
+            if pivot == '0':
+                num = ['0']
+                num += s[:len(s) - 1]
+                s = num
+            # Odd Number
+            else:
+                s[-1] = '0'
+                ind = len(s) - 2
+                while ind >= 0:
+                    if s[ind] == '0':
+                        s[ind] = '1'
+                        break
+                    s[ind] = "0"
+                    ind -= 1
+                
+                if ind == -1:
+                    s = ['1'] + s
+
+            print(s)
+            count += 1
+
+        return count 
