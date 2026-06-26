c20d5db562839387fe47e59e9fb49aa1781cc227
Lohith Bellad <lohith.pre@gmail.com>
2026-06-26 12:22:26 -0700

    Time: 0 ms (100%), Space: 19.4 MB (22.78%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,12 +1,11 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        """
+
         MOD = (7 + 10**9)
         even_count = (n + 1) // 2
         odd_count = n - even_count
 
-        out = (5**even_count) % MOD
-        out *= (4**odd_count) % MOD
+        out = pow(5,even_count, MOD) * pow(4,odd_count, MOD)
 
         return out % MOD
         """
@@ -14,3 +13,4 @@ class Solution:
         even = (n + 1) // 2   # positions 0,2,4,...
         odd  = n // 2         # positions 1,3,5,...
         return (pow(5, even, MOD) * pow(4, odd, MOD)) % MOD
+        """
