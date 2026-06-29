e00f90006854bcf76829a95b5ecd8641f7814794
Lohith Bellad <lohith.pre@gmail.com>
2026-06-28 18:29:45 -0700

    Time: 7092 ms (5.07%), Space: 48.3 MB (38.99%) - LeetHub

---
+++ 3453-separate-squares-i/3453-separate-squares-i.py
@@ -0,0 +1,32 @@
+class Solution:
+    def separateSquares(self, squares: List[List[int]]) -> float:
+        def area_below(y: float) -> float:
+            area = 0.0
+
+            for _, yi, h in squares:
+                ylen = min(float(h), max(0.0, y - yi))
+                area += ylen * h
+            
+            return area
+        
+        total_area = 0.0
+        min_y = float("inf")
+        max_y = float("-inf")
+
+        for _, y, h in squares:
+            total_area += h * h
+            min_y = min(min_y, y)
+            max_y = max(max_y, y + h)
+        
+        target_area = total_area / 2.0
+
+        for i in range(100):
+            mid = min_y + (max_y - min_y) / 2.0
+
+            a = area_below(mid)
+            if a < target_area:
+                min_y = mid
+            else:
+                max_y = mid
+        
+        return min_y
