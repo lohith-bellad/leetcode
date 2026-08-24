46687072a515f0bb5ce41c9e5ad3ea00f6c49541
Lohith Bellad <lohith.pre@gmail.com>
2026-08-24 15:03:50 -0700

    Time: 579 ms (39.74%), Space: 19.6 MB (52.78%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,5 +1,6 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        """
         def traverse(cur_amt: int):
             if cur_amt in cache:
                 return cache[cur_amt]
@@ -21,3 +22,16 @@ class Solution:
             return -1
         
         return output
+        """
+        dp = [float('inf') for i in range(amount + 1)]
+        dp[0] = 0
+
+        for amt in range(1, amount + 1):
+            for coin in coins:
+                if amt - coin >= 0:
+                    dp[amt] = min(dp[amt], 1 + dp[amt - coin])
+
+        if dp[amount] == float('inf'):
+            return -1
+        
+        return dp[amount]
