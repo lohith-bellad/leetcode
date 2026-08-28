c7cf123ab0b21e8e1e6a449fcd6575970c37ece3
Lohith Bellad <lohith.pre@gmail.com>
2026-08-28 14:22:55 -0700

    Time: 3804 ms (5.03%), Space: 41.4 MB (19.95%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -1,19 +1,19 @@
 class Solution:
     def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
-        def dfs(item, taken):
+        def dfs(item):
             if item in taken:
                 return False
 
             if item not in mapping:
                 return False
             
-            if item in output:
+            if item in cache:
                 return True
 
             taken.add(item)
 
             for needed in mapping[item]:
-                if not dfs(needed, taken):
+                if not dfs(needed):
                     return False
             
             taken.remove(item)
@@ -22,16 +22,18 @@ class Solution:
 
         mapping = {}
         n = len(recipes)
-        output = []
 
         for i in range(n):
             mapping[recipes[i]] = ingredients[i]
-            for ing in ingredients[i]:
-                if ing in supplies:
-                    mapping[ing] = []
-        
+
+        for s in supplies:
+            mapping[s] = []
+
+        cache = {}
+        output = []
+        taken = set()
         for recipe in recipes:
-            if dfs(recipe, set()):
+            if dfs(recipe):
                 output.append(recipe)
 
         return output
