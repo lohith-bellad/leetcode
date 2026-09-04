0c707bc590428a438cd0eb49e6a8e79eeaf09abc
Lohith Bellad <lohith.pre@gmail.com>
2026-09-04 11:27:33 -0700

    Time: 12 ms (75.39%), Space: 19.5 MB (58.92%) - LeetHub

---
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -0,0 +1,39 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        output = []
+
+        for num in nums:
+            ind = bisect.bisect_left(output, num)
+            if ind == len(output):
+                output.append(num)
+            else:
+                output[ind] = num
+        
+        return len(output)
+        """
+        def find_ind(num):
+            start = 0
+            end = len(output)
+
+            while start < end:
+                mid = start + (end - start) // 2
+
+                if output[mid] < num:
+                    start = mid + 1
+                else:
+                    end = mid
+
+            return end
+        
+        output = []
+
+        for num in nums:
+            ind = find_ind(num)
+
+            if ind == len(output):
+                output.append(num)
+            else:
+                output[ind] = num
+
+        return len(output)
