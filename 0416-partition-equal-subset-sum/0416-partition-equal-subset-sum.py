12c461b1245ca59faef8f2944987a4ba360c6ee3
Lohith Bellad <lohith.pre@gmail.com>
2026-09-05 19:03:49 -0700

    Time: 2147 ms (14.88%), Space: 487.9 MB (5.34%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -0,0 +1,51 @@
+class Solution:
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        
+        def dfs(target: int, ind: int, cache: []) -> bool:
+            if target == 0:
+                return True
+            
+            if ind == 0 or target < 0:
+                return False
+            
+            if cache[ind][target] != -1:
+                return cache[ind][target]
+
+            cache[ind][target] = dfs(target - nums[ind], ind - 1, cache) or dfs(target, ind - 1, cache)
+            return cache[ind][target]
+
+        total_sum = sum(nums)
+        if total_sum % 2 == 1:
+            return False
+        
+        target = total_sum // 2
+        cache = [[-1] * (target + 1) for i in range(len(nums)+1)]
+
+        res =  dfs(target, len(nums) - 1, cache)
+        print(cache)
+        return res
+        """
+        def dfs(ind, cur_sum):
+            if ind == len(nums):
+                if cur_sum == target_sum:
+                    return True
+                return False
+
+            if cur_sum > target_sum:
+                return False
+            
+            if (ind, cur_sum) in cache:
+                return cache[(ind, cur_sum)]
+
+            cache[(ind, cur_sum)] = dfs(ind + 1, cur_sum) or dfs(ind + 1, cur_sum + nums[ind])
+            return cache[(ind, cur_sum)]
+            
+        target_sum = sum(nums) // 2 
+        if sum(nums) % 2 == 1:
+            return False
+
+        cache = {}
+        return dfs(0, 0)
