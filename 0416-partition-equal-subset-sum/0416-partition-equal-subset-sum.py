a37c0c74bfdf69ba903578c34066d89582454dbb
Lohith Bellad <lohith.pre@gmail.com>
2026-09-05 21:33:05 -0700

    Time: 794 ms (46.41%), Space: 160.8 MB (16.88%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,32 +1,5 @@
 class Solution:
     def canPartition(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: bool
-        
-        def dfs(target: int, ind: int, cache: []) -> bool:
-            if target == 0:
-                return True
-            
-            if ind == 0 or target < 0:
-                return False
-            
-            if cache[ind][target] != -1:
-                return cache[ind][target]
-
-            cache[ind][target] = dfs(target - nums[ind], ind - 1, cache) or dfs(target, ind - 1, cache)
-            return cache[ind][target]
-
-        total_sum = sum(nums)
-        if total_sum % 2 == 1:
-            return False
-        
-        target = total_sum // 2
-        cache = [[-1] * (target + 1) for i in range(len(nums)+1)]
-
-        res =  dfs(target, len(nums) - 1, cache)
-        return res
-        """
         def dfs(ind, cur_sum):
             if ind == len(nums):
                 if cur_sum == target_sum:
