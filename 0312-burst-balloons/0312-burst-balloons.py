0aff1ecb4574acb8093b1336ef112a37af172fcf
Lohith Bellad <lohith.pre@gmail.com>
2026-09-06 15:30:16 -0700

    Time: 2057 ms (71.91%), Space: 22 MB (92.22%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
+        """
         def dfs(start, end):
             if end - start < 2:
                 return 0
@@ -18,3 +19,16 @@ class Solution:
         nums = [1] + nums + [1]
         cache = {}
         return dfs(0, len(nums) - 1)
+        """
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp = [[0 for i in range(n)] for i in range(n)]
+
+        for gap in range(2, n):
+            for start in range(n - gap):
+                end = start + gap
+                for k in range(start + 1, end):
+                    cur_val = nums[start] * nums[k] * nums[end]
+                    dp[start][end] = max(dp[start][end], dp[start][k] + dp[k][end] + cur_val)
+
+        return dp[0][n-1]
