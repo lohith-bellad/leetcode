43b97e914350190e8dee8f42d6236e32c1e389fb
Lohith Bellad <lohith.pre@gmail.com>
2026-09-06 18:35:29 -0700

    Time: 1123 ms (9.58%), Space: 20.3 MB (95.74%) - LeetHub

---
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
@@ -0,0 +1,19 @@
+class MRUQueue:
+
+    def __init__(self, n: int):
+        self.nums = [i for i in range(1, n + 1)]
+
+    def fetch(self, k: int) -> int:
+        elem = self.nums[k - 1]
+
+        for i in range(k, len(self.nums)):
+            self.nums[i - 1] = self.nums[i]
+        
+        self.nums[-1] = elem
+        return elem
+        
+
+
+# Your MRUQueue object will be instantiated and called as such:
+# obj = MRUQueue(n)
+# param_1 = obj.fetch(k)
