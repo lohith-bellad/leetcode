6b30979a1ce960715bdab6bea01aaf7acfcb6a68
Lohith Bellad <lohith.pre@gmail.com>
2026-09-06 18:49:31 -0700

    Time: 83 ms (55.32%), Space: 20.8 MB (42.55%) - LeetHub

--- 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
+++ 1756-design-most-recently-used-queue/1756-design-most-recently-used-queue.py
@@ -1,5 +1,5 @@
 class MRUQueue:
-
+    """
     def __init__(self, n: int):
         self.nums = [i for i in range(1, n + 1)]
 
@@ -11,6 +11,16 @@ class MRUQueue:
         
         self.nums[-1] = elem
         return elem
+    """
+    def __init__(self, N: int):
+        self.nums = SortedList((v, v) for v in range(1, N + 1))
+        
+    def fetch(self, k: int) -> int:
+        res = self.nums[k - 1][1]
+        last_pos = self.nums[-1][0]
+        del self.nums[k - 1]
+        self.nums.add((last_pos + 1, res))
+        return res
 # Your MRUQueue object will be instantiated and called as such:
 # obj = MRUQueue(n)
 # param_1 = obj.fetch(k)
