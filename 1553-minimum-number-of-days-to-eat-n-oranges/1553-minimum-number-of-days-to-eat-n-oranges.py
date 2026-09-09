8db1acf5e76376538dc29628bf7990d79988f0ff
Lohith Bellad <lohith.pre@gmail.com>
2026-09-08 23:04:44 -0700

    Time: 3 ms (97.27%), Space: 20.3 MB (38.25%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -1,26 +1,18 @@
 class Solution:
     def minDays(self, n: int) -> int:
-        """
         def dfs(rem_oranges):
-            if rem_oranges == 0:
+            if rem_oranges <= 1:
                 return rem_oranges
             
             if rem_oranges in cache:
                 return cache[rem_oranges]
 
-            eat_single = dfs(rem_oranges - 1)
+            #eat_single = dfs(rem_oranges - 1)
 
-            eat_half = float('inf')
-            if rem_oranges % 2 == 0:
-                eaten = rem_oranges / 2
-                eat_half = dfs(rem_oranges - eaten)
+            eat_half = rem_oranges % 2 + dfs(rem_oranges // 2)
+            eat_two_third = rem_oranges % 3 + dfs(rem_oranges // 3)
             
-            eat_two_third = float('inf')
-            if rem_oranges % 3 == 0:
-                eaten = 2 * rem_oranges / 3
-                eat_two_third = dfs(rem_oranges - eaten)
-            
-            cache[rem_oranges] = min(eat_single, eat_half, eat_two_third) + 1
+            cache[rem_oranges] = min(eat_half, eat_two_third) + 1
             return cache[rem_oranges]
 
         cache = {}
@@ -44,3 +36,4 @@ class Solution:
 
         cache = {}
         return dfs(n)
+        """
