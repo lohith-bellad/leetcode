e3e01f5e2599a722cb5d10612462f86f3807b31b
Lohith Bellad <lohith.pre@gmail.com>
2026-09-08 23:05:00 -0700

    Time: 3 ms (97.27%), Space: 20.2 MB (59.56%) - LeetHub

--- 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -7,8 +7,6 @@ class Solution:
             if rem_oranges in cache:
                 return cache[rem_oranges]
 
-            #eat_single = dfs(rem_oranges - 1)
-
             eat_half = rem_oranges % 2 + dfs(rem_oranges // 2)
             eat_two_third = rem_oranges % 3 + dfs(rem_oranges // 3)
             
@@ -17,23 +15,3 @@ class Solution:
 
         cache = {}
         return dfs(n)
-        """
-        
-        def dfs(rem_oranges):
-            if rem_oranges <= 1:
-                return rem_oranges
-
-            if rem_oranges in cache:
-                return cache[rem_oranges]
-
-            # eat rem % 2 singles to reach a multiple of 2, then halve
-            eat_half = rem_oranges % 2 + dfs(rem_oranges // 2)
-            # eat rem % 3 singles to reach a multiple of 3, then eat two-thirds
-            eat_two_third = rem_oranges % 3 + dfs(rem_oranges // 3)
-
-            cache[rem_oranges] = min(eat_half, eat_two_third) + 1
-            return cache[rem_oranges]
-
-        cache = {}
-        return dfs(n)
-        """
