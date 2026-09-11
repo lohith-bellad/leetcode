6517b4ae78fa8548b202b61dc6e3b5f6a854fa16
Lohith Bellad <lohith.pre@gmail.com>
2026-09-10 21:56:44 -0700

    Time: 3334 ms (5.04%), Space: 67.1 MB (6.47%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        def dfs(unit, jump):
+            if unit == stones[-1]:
+                return True
+
+            if unit not in stones:
+                return False
+
+            if (unit, jump) in cache:
+                return cache[(unit, jump)]
+
+            long_jump = dfs(unit + jump + 1, jump + 1)
+
+            short_jump = False
+            if jump != 1:
+                short_jump = dfs(unit + jump - 1, jump - 1)
+            
+            med_jump = dfs(unit + jump, jump)
+
+            cache[(unit, jump)] = long_jump or short_jump or med_jump
+            return cache[(unit, jump)]
+        
+        cache = {}
+        return dfs(stones[0] + 1, 1)
