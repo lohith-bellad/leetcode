8e9050901790dce53dcc5efc5f1b9e62545218ef
Lohith Bellad <lohith.pre@gmail.com>
2026-09-10 23:20:32 -0700

    Time: 757 ms (5.01%), Space: 183 MB (6.24%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,40 +1,43 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        m, n = len(str1), len(str2)
-        cache = {}
-
-        def dfs(i, j):
-            if i == m:
-                return n - j
-            if j == n:
-                return m - i
-            if (i, j) in cache:
-                return cache[(i, j)]
-
-            if str1[i] == str2[j]:
-                res = 1 + dfs(i + 1, j + 1)
+        def dfs(p, q):
+            if p == len(str1) and q == len(str2):
+                return 0
+                
+            if p == len(str1):
+                return len(str2) - q
+            if q == len(str2):
+                return len(str1) - p
+            
+            if (p, q) in cache:
+                return cache[(p, q)]
+            
+            if str1[p] == str2[q]:
+                res = 1 + dfs(p + 1, q + 1)
             else:
-                res = 1 + min(dfs(i + 1, j), dfs(i, j + 1))
-
-            cache[(i, j)] = res
+                res = 1 + min(dfs(p, q + 1), dfs(p + 1, q))
+            
+            cache[(p, q)] = res
             return res
 
-        dfs(0, 0)                      # fill the memo
+        output = []
+        i = 0
+        j = 0
+        cache = {}
 
-        out = []
-        i = j = 0
-        while i < m and j < n:         # walk forward, memo answers each choice
+        while i < len(str1) and j < len(str2):
             if str1[i] == str2[j]:
-                out.append(str1[i])
+                output.append(str1[i])
                 i += 1
                 j += 1
             elif dfs(i + 1, j) <= dfs(i, j + 1):
-                out.append(str1[i])
+                output.append(str1[i])
                 i += 1
             else:
-                out.append(str2[j])
+                output.append(str2[j])
                 j += 1
+        
+        output.append(str1[i:])
+        output.append(str2[j:])
 
-        out.append(str1[i:])           # tail drain, same trap as bottom-up
-        out.append(str2[j:])
-        return "".join(out)
+        return "".join(output)
