7f1a458dd1671ad2889069f2eac53f66d189a872
Lohith Bellad <lohith.pre@gmail.com>
2026-09-19 16:14:46 -0700

    Time: 111 ms (6.56%), Space: 24.7 MB (12.57%) - LeetHub

--- 0265-paint-house-ii/0265-paint-house-ii.py
+++ 0265-paint-house-ii/0265-paint-house-ii.py
@@ -1,5 +1,6 @@
 class Solution:
     def minCostII(self, costs: List[List[int]]) -> int:
+        """
         row_max = len(costs)
         col_max = len(costs[0])
 
@@ -26,3 +27,29 @@ class Solution:
             r = min(r, dfs(0, col))
         
         return r
+        """
+        def dfs(h_ind, c_ind):
+            if h_ind == max_houses:
+                return 0
+
+            if (h_ind, c_ind) in cache:
+                return cache[(h_ind, c_ind)]
+
+            r = float('inf')
+            for c in range(max_colors):
+                if c == c_ind:
+                    continue
+                r = min(r, dfs(h_ind + 1, c) + costs[h_ind][c_ind])
+
+            cache[(h_ind, c_ind)] = r
+            return r
+        
+        max_houses = len(costs)
+        max_colors = len(costs[0])
+
+        cache = {}
+        res = float('inf')
+        for c in range(max_colors):
+            res = min(res, dfs(0, c))
+
+        return res
